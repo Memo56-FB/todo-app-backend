a0040c1072b7af04ce05b149c9f17e52a4d74ad1
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-15 12:41:42 -0500

    feat: heroku config

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.delete('/api/todo/:id', (req, res) => {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log('Server running on port: ', PORT)
