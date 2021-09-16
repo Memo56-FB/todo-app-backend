d68ece97b1ac01faccc4965ec32d7dac9451b906
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-16 12:16:46 -0500

    refactor: change todoSchema toJSON

--- models/todoSchema.js
+++ models/todoSchema.js
@@ -6,6 +6,14 @@ const todoSchema = new mongoose.Schema({
   complete: Boolean
 })
 
+todoSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id
+    delete ret._id
+    delete ret.__v
+  }
+})
+
 const Todo = mongoose.model('Todo', todoSchema)
 
 module.exports = { Todo }
