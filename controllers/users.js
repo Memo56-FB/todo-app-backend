68e0cfd9b2e31f95bc713b120391c9c7e12cb9fa
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-26 12:53:20 -0500

    feat: add populate to users

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,10 @@ const userRouter = require('express').Router()
 const { User } = require('../models/user')
 
 userRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('todos', {
+    content: 1,
+    date: 1
+  })
   response.json(users)
 })
 
