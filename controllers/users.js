829967aed97df51f0c018f26471054c0324c6453
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-25 23:33:57 -0500

    feat: add get to users

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
 const { User } = require('../models/user')
 
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 userRouter.post('/', async (request, response, next) => {
   try {
     const { body } = request
