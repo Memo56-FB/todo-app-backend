8f4b36a0351349a63b6f33cc4925d71e4873c29f
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-26 18:41:23 -0500

    refactor: change promises to async functions

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,51 +2,60 @@ const todoRouter = require('express').Router()
 const { Todo } = require('../models/todoSchema')
 const { User } = require('../models/user')
 
-todoRouter.get('/', (req, res, next) => {
-  Todo.find({})
-    .then(todo => res.json(todo))
-    .catch(error => next(error))
+todoRouter.get('/', async (req, res, next) => {
+  try {
+    const todos = await Todo.find({})
+    res.json(todos)
+  } catch (error) {
+    next(error)
+  }
 })
-todoRouter.get('/:id', (req, res, next) => {
-  const id = req.params.id
-  Todo.findById(id)
-    .then(todo => res.status(200).send(todo))
-    .catch(error => next(error))
+todoRouter.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const todo = await Todo.findById(id)
+    res.status(200).send(todo)
+  } catch (error) {
+    next(error)
+  }
 })
 
 todoRouter.post('/', async (req, res, next) => {
-  const { body } = req
-  const { content, complete, userId } = body
-  const user = await User.findById(userId)
-  if (!body || !content) {
-    res.status(400).json({ error: 'content missing' })
-  } else {
-    const newTodo = new Todo({
-      content,
-      date: new Date(),
-      complete,
-      user: user._id
-    })
-    try {
+  try {
+    const { body } = req
+    const { content, complete, userId } = body
+    const user = await User.findById(userId)
+    if (!body || !content) {
+      res.status(400).json({ error: 'content missing' })
+    } else {
+      const newTodo = new Todo({
+        content,
+        date: new Date(),
+        complete,
+        user: user._id
+      })
       const savedTodo = await newTodo.save()
 
       user.todos = user.todos.concat(savedTodo._id)
       await user.save()
 
       res.status(201).json(savedTodo)
-    } catch (error) {
-      next(error)
     }
+  } catch (error) {
+    next(error)
   }
 })
 
-todoRouter.delete('/:id', (req, res, next) => {
-  const id = req.params.id
-  Todo.findByIdAndRemove(id)
-    .then(result => res.status(204).end())
-    .catch(error => next(error))
+todoRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    await Todo.findByIdAndRemove(id)
+    res.status(204).end()
+  } catch (error) {
+    next(error)
+  }
 })
-todoRouter.put('/:id', (req, res, next) => {
+todoRouter.put('/:id', async (req, res, next) => {
   const id = req.params.id
   const body = req.body
   const { content, complete } = body
@@ -66,9 +75,12 @@ todoRouter.put('/:id', (req, res, next) => {
       complete
     }
   }
-  Todo.findByIdAndUpdate(id, todo, { new: true })
-    .then(result => res.status(200).json(result))
-    .catch(error => next(error))
+  try {
+    const todoUpdated = await Todo.findByIdAndUpdate(id, todo, { new: true })
+    res.status(200).json(todoUpdated)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = { todoRouter }
