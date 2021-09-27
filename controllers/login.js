e0f9d9af780878bd08daea68ae0d1a3c39345a66
Guillermo Farfan <memo56_fb@hotmail.com>
2021-09-27 14:40:32 -0500

    feat: add json web token

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,8 @@
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
+const jsonWebToken = require('jsonwebtoken')
+
+require('dotenv').config()
 
 const { User } = require('../models/user')
 
@@ -12,12 +15,19 @@ loginRouter.post('/', async (request, response) => {
     ? false
     : await bcrypt.compare(password, user.passwordHash)
 
-  if (!passwordCorrect) {
+  if (!user && !passwordCorrect) {
     response.status(401).json({ error: 'invalid user or password' })
   }
+
+  const userForToken = {
+    id: user._id,
+    username: user.username
+  }
+  const token = jsonWebToken.sign(userForToken, process.env.SECRET)
   response.send({
     name: user.name,
-    username: user.username
+    username: user.username,
+    token
   })
 })
 
