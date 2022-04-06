96e392a8871bce33a5ba0cff8014ab075ee7d95d
Guillermo Farfan Bivanco <32189115+Memo56-FB@users.noreply.github.com>
2022-04-06 00:33:50 -0500

    fix(login): change user and password validator

--- controllers/login.js
+++ controllers/login.js
@@ -15,7 +15,7 @@ loginRouter.post('/', async (request, response) => {
     ? false
     : await bcrypt.compare(password, user.passwordHash)
 
-  if (!user && !passwordCorrect) {
+  if (!user || !passwordCorrect) {
     response.status(401).json({ error: 'invalid user or password' })
   }
 
